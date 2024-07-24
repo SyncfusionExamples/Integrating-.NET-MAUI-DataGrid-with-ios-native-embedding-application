01bacfc84a13b55b077383c5ca97d2ca445ac9dc
SethupathyD <sethupathy.d@syncfusion.com>
2024-07-24 15:03:57 +0530

    Code updated

---
+++ SfDataGridIOSSample/SfDataGridIOSSample/SfDataGridIOSSample/Main.cs
@@ -0,0 +1,8 @@
+﻿using SfDataGridIOSSample;
+
+
+// This is the main entry point of the application.
+// If you want to use a different Application Delegate class from "AppDelegate"
+// you can specify it here.
+UIKit.UIApplication.Main (args, null, typeof (AppDelegate));
+
